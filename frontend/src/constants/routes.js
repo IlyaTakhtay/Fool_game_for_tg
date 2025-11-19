cafb42b68107841546d4046615bfc11988799f03
Ilya <i.tahtaj@gmail.com>
2025-11-20 00:23:40 +0300

    DELETE: moved to https://github.com/IlyaTakhtay/Fool_game_for_tg-client

--- frontend/src/constants/routes.js
+++
@@ -1,8 +0,0 @@
-export const ROUTES = {
-    GAMES: '/games',
-    GAME: '/game/:game_id',
-    CREATE_GAME: '/create-game',
-    COMING_SOON: '/coming-soon',
-    AUTH_GUEST: '/auth-guest',
-  };
-  
